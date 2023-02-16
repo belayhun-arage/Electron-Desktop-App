5a036dad69cbe42346b08755b10242f5a87180b5
belayhun-arage <belayhun24@gmail.com>
2023-02-16 15:13:13 +0300

    some modification

--- preload.js
+++ preload.js
@@ -1,9 +1,14 @@
-const { contextBridge, ipcRenderer } = require('electron')
+//runs before web page is loaded into the browser window
+//has access to both DOM APIs and Node.js environment
+const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('versions', {
-    node: () => process.versions.node,
-    chrome: () => process.versions.chrome,
-    electron: () => process.versions.electron,
-    ping:() => ipcRenderer.invoke('ping')//sender
-    // we can also expose variables, not just functions
-})
+contextBridge.exposeInMainWorld(
+    'versions', 
+        {
+            node: () => process.versions.node,
+            chrome: () => process.versions.chrome,
+            electron: () => process.versions.electron,
+            ping:() => ipcRenderer.invoke('ping')//sender
+            // we can also expose variables, not just functions
+        }
+);
